058348be652cc7a4735e127d44eb62905c65bd66
E-Duncan <57486822+E-Duncan@users.noreply.github.com>
2023-08-06 18:07:33 +0100

    Create make_province_loop.py

---
+++ format_data/make_province_loop.py
@@ -0,0 +1,18 @@
+from make_dataframe_function import make_prov_file
+import numpy as np
+
+
+provinces = np.arange(52,53) #52)
+
+#Load in longhurst shape file
+import fiona
+mask_path = '/data/datasets/Projects/TuringCoccolithophoreBlooms/province_files/Longhurst_world_v4_2010.shp'
+shapefile = fiona.open(mask_path)
+with fiona.open(mask_path) as shapefile:
+    shapes = [feature for feature in shapefile]
+
+
+#Loop over provinces
+for p in provinces:
+    geometries = [shapes[p]['geometry']]
+    make_prov_file(p,geometries)
